bdcf97f543d68ac3ccadffca8d103a7e5777d176
luismolinaboni22-art <luismolinaboni22@gmail.com>
2025-12-03 05:35:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print('Hello from K9 app')
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Usuario quemado para prueba
+USERS = {
+    "admin": "1234"
+}
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username in USERS and USERS[username] == password:
+        return redirect(url_for("home"))
+
+    return render_template("login.html", error="Credenciales incorrectas")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
